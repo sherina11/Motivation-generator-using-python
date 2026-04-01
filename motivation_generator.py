1c3e828b70d38c0364eaadaa04c8174305bba734
Raja Sherina T <rajasherinaa@gmail.com>
2026-04-01 22:07:09 +0530

    Added motivation generator python program

---
+++ motivation_generator.py
@@ -0,0 +1,16 @@
+import random
+
+quotes = [
+    "Believe in yourself!",
+    "Never give up.",
+    "Every day is a new beginning.",
+    "Dream big and work hard.",
+    "Success comes with practice.",
+    "Stay positive and strong."
+]
+
+print("\n🌟 Your Motivation for Today 🌟")
+
+quote = random.choice(quotes)
+
+print(quote)
